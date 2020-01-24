cbec607365e3bc69a4ca787618768c421e0c3ab5
vishal <narwade.v.v@gmail.com>
2020-01-24 18:51:53 +0530

    Token on login

--- DatingApp/Controllers/AuthController.cs
+++ DatingApp/Controllers/AuthController.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using DatingApp.Data;
 using DatingApp.Model;
+using DatingApp.DTO;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace DatingApp.Controllers
 {
@@ -14,22 +20,49 @@ namespace DatingApp.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
-
+            _config = config;
         }
         [HttpPost("register")]
-        
-        public async Task<IActionResult> Register(string username, string password)
+
+        public async Task<IActionResult> Register(UserForRgisterDto userForRgisterDto)
         {
-            username = username.ToLower();
-            if (await _repo.UserExists(username))
+            userForRgisterDto.username = userForRgisterDto.username.ToLower();
+            if (await _repo.UserExists(userForRgisterDto.username))
                 return BadRequest("Username is already exists");
 
-            var userToCreate = new User { Username = username };
-            var createdUser = await _repo.Register(userToCreate, password);
+            var userToCreate = new User { Username = userForRgisterDto.username };
+            var createdUser = await _repo.Register(userToCreate, userForRgisterDto.password);
             return StatusCode(201);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.username.ToLower(), userForLoginDto.password);
+            if (userFromRepo == null)
+                return Unauthorized();
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name,userFromRepo.Username)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new { token = tokenHandler.WriteToken(token) });
         }
     }
 }
---
+++ DatingApp/DTO/UserForLoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.DTO
+{
+    public class UserForLoginDto
+    {
+        [Required]
+        public string username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4-8 characters")]
+        public string password { get; set; }
+    }
+}
